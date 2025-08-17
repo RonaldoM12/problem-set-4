c9d9356e0e157d9da2d4e8c9574a6c238a540102
RonaldoM12 <rmedina1@umd.edu>
2025-08-17 15:37:00 -0400

    Add files via upload

---
+++ part3_similar_actors_genre.py
@@ -0,0 +1,56 @@
+'''
+PART 2: SIMILAR ACTROS BY GENRE
+
+Using the imbd_movies dataset:
+- Create a data frame, where each row corresponds to an actor, each column represents a genre, and each cell captures how many times that row's actor has appeared in that column’s genre 
+- Using this data frame as your “feature matrix”, select an actor (called your “query”) for whom you want to find the top 10 most similar actors based on the genres in which they’ve starred 
+- - As an example, select the row from your data frame associated with Chris Hemsworth, actor ID “nm1165110”, as your “query” actor
+- Use sklearn.metrics.DistanceMetric to calculate the euclidean distances between your query actor and all other actors based on their genre appearances
+- - https://scikit-learn.org/stable/modules/generated/sklearn.metrics.DistanceMetric.html
+- Output a CSV continaing the top ten actors most similar to your query actor using cosine distance 
+- - Name it 'similar_actors_genre_{current_datetime}.csv' to `/data`
+- - For example, the top 10 for Chris Hemsworth are:  
+        nm1165110 Chris Hemsworth
+        nm0000129 Tom Cruise
+        nm0147147 Henry Cavill
+        nm0829032 Ray Stevenson
+        nm5899377 Tiger Shroff
+        nm1679372 Sudeep
+        nm0003244 Jordi Mollà
+        nm0636280 Richard Norton
+        nm0607884 Mark Mortimer
+        nm2018237 Taylor Kitsch
+- Describe in a print() statement how this list changes based on Euclidean distance
+- Make sure your code is in line with the standards we're using in this class
+'''
+import pandas as pd
+import json
+from sklearn.metrics import DistanceMetric
+from datetime import datetime
+
+#Write your code below
+
+def similar_actors_genre(data, query_id, top_10 = 10):
+    
+    data = pd.read_json("imdb_movies.json")
+
+    names = data["name"]
+
+    feature = data.drop(columns=["name"])
+
+    query_id = "nm1165110" #Chris Hemsworth ID number
+
+    query = feature.loc[query_id].values.reshape(1, -1)
+
+    euclidean = DistanceMetric.get_metric("Euclidean")
+
+    distances = euclidean.pairwise(query, feature.values)[0]
+
+    result = pd.DataFrame({
+        "actor_id": feature.index,
+        "name": names,
+        "euclidean_dist": distances
+    }).sort_values(by="euclidean_dist")
+
+
+    return result
